d1d4608b21f5ad2daa2feec44bc3d81abc7abe3d
akashvaithemal <sahmmed974@gmail.com>
2022-03-01 20:02:14 +0600

    search result html added and showing result using js added

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,24 @@
-console.log('added')
+// console.log('added')
+// input catching 
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText)
+    // clearing input section value 
+    searchField.value ='';
+    // getting data 
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+// console.log(url)
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResult(data.data))
+}
+
+// showing search result 
+const displaySearchResult = data => {
+    // console.log(data)
+    const searchResult = document.getElementById('search-result');
+    data.forEach(data => {
+        console.log(data);
+    });
+}
